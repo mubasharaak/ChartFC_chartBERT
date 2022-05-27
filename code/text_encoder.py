979ad37b7b13060e5909c9c88379dc27a6758e6f
Mubashara Akhtar <mubashara.akhtar@hotmail.com>
2022-05-27 08:13:29 +0100

    Adjusted BertEncoder to have a 3-dim output

--- code/text_encoder.py
+++ code/text_encoder.py
@@ -82,6 +82,8 @@ class BertEncoder(TextEncoder):
                                                       return_attention_mask=True)
         embeddings = embeddings.to('cuda')
         out = self.bert_encoder(**embeddings)
-        txt_feat = torch.mean(out[0], dim=1).squeeze()
+        txt_feat = torch.mean(out[0], dim=1)
+        txt_feat = torch.unsqueeze(txt_feat, dim=1)
 
+        print(f"BertEncoder: {txt_feat.shape}")
         return txt_feat
